d5bd0f19fce5410240303dec6f6a7a13315b526c
TuyenLN <tuyenlnse@gmail,com>
2022-03-21 16:56:46 +0700

    add customer address eav attribute

--- app/code/Mage2tv/EavCustomer/Setup/InstallData.php
+++ app/code/Mage2tv/EavCustomer/Setup/InstallData.php
@@ -3,7 +3,9 @@
 namespace Mage2tv\EavCustomer\Setup;
 
 
+use Magento\Customer\Api\AddressMetadataInterface;
 use Magento\Customer\Api\CustomerMetadataInterface;
+use Magento\Eav\Setup\EavSetup;
 use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Eav\Model\Config as EavConfig;
 use Magento\Framework\Setup\InstallDataInterface;
@@ -30,7 +32,71 @@ class InstallData implements InstallDataInterface
 
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
+        $setup->startSetup();
         $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $this->addCustomerInterestsAttribute($eavSetup);
+        $attributeCode = 'is_home_address';
+        $entityType = AddressMetadataInterface::ENTITY_TYPE_ADDRESS;
+
+        $eavSetup->addAttribute($entityType, $attributeCode, [
+            'label' => 'Is home address?',
+            'type' => 'int',
+            'input' => 'boolean',
+            'required' => 0,
+            'user_defined' => 1,
+            'default'   => 0,
+            'system' => 0,
+            'position' => 50,
+        ]);
+
+        $eavSetup->addAttributeToSet(
+            $entityType,
+            AddressMetadataInterface::ATTRIBUTE_SET_ID_ADDRESS,
+            null,
+            $attributeCode
+        );
+        $attribute = $this->eavConfig->getAttribute($entityType, $attributeCode);
+        $attribute->setData('used_in_forms', [
+            'adminhtml_customer_address',
+            'customer_address_edit',
+            'customer_register_address'
+        ]);
+        $attribute->save();
+    }
+
+    private function addEavCustomerAddressAttribute(eavSetup $eavSetup)
+    {
+//        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $attributeCode = 'is_home_address';
+        $entityType = AddressMetadataInterface::ENTITY_TYPE_ADDRESS;
+
+        $eavSetup->addAttribute($entityType, $attributeCode, [
+            'label' => 'Is home address?',
+            'type' => 'int',
+            'input' => 'boolean',
+            'required' => 0,
+            'user_defined' => 1,
+            'default'   => 0,
+            'system' => 0,
+            'position' => 50,
+        ]);
+
+        $eavSetup->addAttributeToSet(
+            $entityType,
+            AddressMetadataInterface::ATTRIBUTE_SET_ID_ADDRESS,
+            null,
+            $attributeCode
+        );
+        $attribute = $this->eavConfig->getAttribute($entityType, $attributeCode);
+        $attribute->setData('used_in_forms', [
+            'adminhtml_customer_address',
+            'customer_address_edit',
+            'customer_register_address'
+        ]);
+        $attribute->save();
+    }
+
+    private function addCustomerInterestsAttribute(EavSetup $eavSetup) {
         $attributeCode = 'interests';
         $entityType = CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER;
 
@@ -57,6 +123,5 @@ class InstallData implements InstallDataInterface
             'customer_acount_edit'
         ]);
         $attribute->save();
-//        $attribute->getResource()->save($attribute);
     }
 }

b3fda90538ba962b3cf7bd4f82dae750e61da3e6
TuyenLN <tuyenlnse@gmail,com>
2022-03-21 13:32:09 +0700

    init module add EAV attribute to category

---
+++ app/code/Mage2tv/EavCategory/Setup/InstallData.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Mage2tv\EavCategory\Setup;
+
+
+use Magento\Catalog\Api\Data\CategoryAttributeInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $attributeCode = 'external_id';
+        $entityType = CategoryAttributeInterface::ENTITY_TYPE_CODE;
+
+        $eavSetup->addAttribute($entityType, $attributeCode, [
+            'label' => 'External Id',
+            'user_defined' => 1,
+            'unique' => 1
+        ]);
+
+        $setId = $eavSetup->getDefaultAttributeSetId($entityType);
+        $groupId = $eavSetup->getDefaultAttributeGroupId($entityType, $setId);
+        $eavSetup->addAttributeToSet($entityType, $setId, $groupId, $attributeCode);
+    }
+}
---
+++ app/code/Mage2tv/EavCategory/Setup/UpgradeSchema.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mage2tv\EavCategory\Setup;
+
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $setup->endSetup();
+    }
+
+
+}
